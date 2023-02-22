882f4c956886a4f11fc5291b9c582d2422b4522d
JosueBautista <alejandrojhb84@gmail.com>
2023-02-22 17:11:29 -0600

    added get and post method

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
-from typing import Union
+from typing import Union, List
+from uuid import uuid4, UUID
 from fastapi import FastAPI
+from models import *
+
 
 app = FastAPI()
 
+db: List[User] = [
+    User(
+    id = UUID("040d5d7d-6d50-4192-9e90-11ae5de6d906"), 
+    first_name="Jamilla", 
+    last_name="Ahmed",
+    gender=Gender.male,
+    roles=[Role.student]
+    ), 
+    User(
+    id = UUID("9b8c33bf-4e1f-41e8-821a-8700e8a5cddf"), 
+    first_name="Alex", 
+    last_name="Jones",
+    gender=Gender.female,
+    roles=[Role.admin, Role.user]
+    ), 
+]
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
+# get one item
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+#get all items
+@app.get('/api/v1/users')
+async def fetch_users():
+    return db
+
+#post an item
+@app.post('/api/v1/users')
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
---
+++ models.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from uuid import UUID, uuid4
+from enum import Enum
+from typing import List, Optional
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
+class Role(str, Enum):
+    admin = "admin"
+    user = "user"
+    student = "student" 
+
+class User(BaseModel):
+    id: Optional[UUID] = uuid4()
+    first_name: str
+    middle_name: Optional[str] = ''
+    last_name: str
+    gender: Gender
+    roles: List[Role]
